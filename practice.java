d95aad70d251dc64efcadef1fa220137528e5f2e
Gabriel-Chilatu-Sinkala <120474088+Gabriel-Chilatu-Sinkala@users.noreply.github.com>
2024-01-16 19:46:17 +0200

    First commit

---
+++ practice.java
@@ -0,0 +1,15 @@
+/**
+ * practice
+ */
+public class practice {
+
+    public static void main(String[] args) {
+        double i = 50.0;
+        double k = i + 20;
+        double j = k + i;
+
+        System.out.print("i is "+i);
+        System.out.print("k is "+k);
+        System.out.print("j is "+j);
+    }
+}
